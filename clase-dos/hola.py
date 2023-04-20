482751cac059265f87c2386560b0fffe9b7d182c
VLV1990 <vicbalever@gmail.com>
2023-04-19 22:46:33 -0400

    enlace

---
+++ clase-dos/hola.py
@@ -0,0 +1,11 @@
+print("hola vicente")
+print("esto es una linea nueva")
+
+edad = 50
+eshumano = True
+nivel_de_poder = 5.4 
+
+print(edad,eshumano,nivel_de_poder)
+numero_1 = 1
+numero_2 = 2
+print(numero_1 + numero_2)
