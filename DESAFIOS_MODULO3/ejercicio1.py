8aba6ad0981a64ac7fa44378f9f87c0a21132dba
VLV1990 <vicbalever@gmail.com>
2023-05-05 23:54:46 -0400

    Update ejercicio1.py

--- DESAFIOS_MODULO3/ejercicio1.py
+++ DESAFIOS_MODULO3/ejercicio1.py
@@ -2,7 +2,7 @@ num_a = int(input('ingresa un numero entero: '))
 num_b = int(input('ingresa otro numero entero: '))
 
 suma = num_a + num_b
-#print(f'la suma de ambos numero es : {suma}')
+
 
 print(f'la suma de {num_a} + {num_b} es : {suma}')
 
