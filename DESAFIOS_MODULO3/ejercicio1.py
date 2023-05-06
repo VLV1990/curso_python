0f00daf4427bc6887210ac47684ce42c0129d71c
VLV1990 <vicbalever@gmail.com>
2023-05-05 21:58:02 -0400

    ejercicio_1

---
+++ DESAFIOS_MODULO3/ejercicio1.py
@@ -0,0 +1,8 @@
+num_a = int(input('ingresa un numero entero: '))
+num_b = int(input('ingresa otro numero entero: '))
+
+def sumatoria():
+    resultado = num_a + num_b
+    return resultado
+
+print(sumatoria())
